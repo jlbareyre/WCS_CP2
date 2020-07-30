aaf15f36a655464b3baef4e5af7407a73b2a7ae1
Jean-Loup BAREYRE <jlbareyre@gmail.com>
2020-07-30 05:40:54 +0200

    Emprunt ajouté

--- src/main/java/com/wcs_cp2/kiakwa/controller/MaterielController.java
+++ src/main/java/com/wcs_cp2/kiakwa/controller/MaterielController.java
@@ -1,9 +1,16 @@
 package com.wcs_cp2.kiakwa.controller;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
+import com.wcs_cp2.kiakwa.model.Emprunt;
 import com.wcs_cp2.kiakwa.model.Materiel;
 import com.wcs_cp2.kiakwa.model.User;
+import com.wcs_cp2.kiakwa.repository.EmpruntRepository;
 import com.wcs_cp2.kiakwa.repository.MaterielRepository;
 import com.wcs_cp2.kiakwa.repository.UserRepository;
 
@@ -24,6 +31,9 @@ public class MaterielController {
 
     @Autowired
     private MaterielRepository materielRepo;
+
+    @Autowired
+    private EmpruntRepository empruntRepo;    
 
     @PostMapping("/materiel")
     public String newMateriel(Model model, @ModelAttribute("userId") String userId,
@@ -54,7 +64,7 @@ public class MaterielController {
     }
     
     @GetMapping("/deleteMateriel/{id}")
-    public String deleteConfigMap(Model model, @PathVariable UUID id, 
+    public String deleteMAteriel(Model model, @PathVariable UUID id, 
                     RedirectAttributes ra) {
 
         Materiel mat = materielRepo.findById(id).get();
@@ -64,6 +74,88 @@ public class MaterielController {
 
         ra.addAttribute("userId", userId);
         return "redirect:/base";
+    }
+        
+    @GetMapping("/materiel/{id}")
+    public String detailMateriel(Model model, @PathVariable UUID id, 
+                    RedirectAttributes ra) {
+
+        Materiel mat = materielRepo.findById(id).get();
+        User user = mat.getUser();
+
+        List<Emprunt> lstEmp = empruntRepo.findAll().stream().filter(e -> mat.equals(e.getMateriel())).collect(Collectors.toList());
+
+        model.addAttribute("materiel", mat);
+        model.addAttribute("emprunts", lstEmp);
+        model.addAttribute("userId", user.getUserID());
+
+        return "materiel";
+    }
+
+    @GetMapping("/emprunt/{id}")
+    public String empruntMateriel(Model model, @PathVariable UUID id, 
+                    RedirectAttributes ra) {
+
+        Materiel mat = materielRepo.findById(id).get();
+        User user = mat.getUser();
+
+        List<Emprunt> lstEmp = empruntRepo.findAll().stream().filter(e -> mat.equals(e.getMateriel())).collect(Collectors.toList());
+
+        model.addAttribute("materiel", mat);
+        model.addAttribute("emprunts", lstEmp);
+        model.addAttribute("userId", user.getUserID());
+
+        return "materiel";
+    }
+
+    @GetMapping("/recupEmprunt/{id}")
+    public String recupEmpruntMateriel(Model model, @PathVariable Long id, 
+                    RedirectAttributes ra) {
+
+        Emprunt emp = empruntRepo.findById(id).get();
+                    
+        Date dt = new Date();
+        emp.setRetour(dt);
+        empruntRepo.save(emp);
+
+        Materiel mat = materielRepo.findById(emp.getMateriel().getMatId()).get();
+        User user = mat.getUser();
+
+        List<Emprunt> lstEmp = empruntRepo.findAll().stream().filter(e -> mat.equals(e.getMateriel())).collect(Collectors.toList());
+
+        model.addAttribute("materiel", mat);
+        model.addAttribute("emprunts", lstEmp);
+        model.addAttribute("userId", user.getUserID());
+
+        return "materiel";
+    }
+
+    @PostMapping("/emprunt")
+    public String nouvelEmpruntMateriel(Model model,
+                    @ModelAttribute("matId") String matId,
+                    @ModelAttribute("qui") String qui,
+                    @ModelAttribute("quand") String quand,
+                    RedirectAttributes ra) {
+
+        Date quandDate;
+		try {
+			quandDate = new SimpleDateFormat("yyyy-MM-dd").parse(quand);
+		} catch (ParseException e1) {
+			quandDate = new Date();
+		}
+        
+        Emprunt emprunt = new Emprunt( qui, quandDate, materielRepo.findById(UUID.fromString(matId)).get() );
+        
+        empruntRepo.save(emprunt);
+
+        Materiel mat = emprunt.getMateriel();
+        List<Emprunt> lstEmp = empruntRepo.findAll().stream().filter(e -> mat.equals(e.getMateriel())).collect(Collectors.toList());
+
+        model.addAttribute("materiel", mat);
+        model.addAttribute("emprunts", lstEmp);
+        model.addAttribute("userId", mat.getUser().getUserID());
+        
+        return "materiel";
     }
 
 
--- src/main/java/com/wcs_cp2/kiakwa/controller/UserController.java
+++ src/main/java/com/wcs_cp2/kiakwa/controller/UserController.java
@@ -12,7 +12,6 @@ import com.wcs_cp2.kiakwa.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
